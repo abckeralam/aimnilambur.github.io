28e6ddd7b7bfae10c143896718ca80f548113e59
abckeralam <70502603+abckeralam@users.noreply.github.com>
2021-04-01 08:04:20 -0700

    Create mark.js

---
+++ 1/mark.js
@@ -0,0 +1,17 @@
+function handleClick()
+          {         
+        var amountCorrect = 0;          
+        for(var i = 1; i <= 100; i++) {
+          var radios = document.getElementsByName(+i);
+          for(var j = 0; j < radios.length; j++) {
+            var radio = radios[j];
+            if(radio.value == "0" && radio.checked) {
+              amountCorrect++;
+            }
+          }
+         }                   
+         document.body.scrollTop = 0; // For Safari
+         document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+         document.getElementById("quiz").innerHTML = "your mark is:" +  amountCorrect +"/"+ 100;
+       
+             }
