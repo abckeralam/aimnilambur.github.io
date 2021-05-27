c020d5cc0f1d043f76c68235217aeaa9cbf17c8c
abckeralam <70502603+abckeralam@users.noreply.github.com>
2021-05-26 22:26:12 -0700

    Update mark.js

--- 1/mark.js
+++ 1/mark.js
@@ -1,17 +1,21 @@
 function handleClick()
           {         
-        var amountCorrect = 0;          
-        for(var i = 1; i <= 100; i++) {
-          var radios = document.getElementsByName(+i);
-          for(var j = 0; j < radios.length; j++) {
-            var radio = radios[j];
-            if(radio.value == "0" && radio.checked) {
-              amountCorrect++;
-            }
+           var amountCorrect = 0;          
+           for(var i = 1; i <= 100; i++) 
+             {
+               var radios = document.getElementsByName(+i);
+               for(var j = 0; j < radios.length; j++)
+                {
+                  var radio = radios[j];
+                  if(radio.value == "0" && radio.checked)
+                   {
+                  amountCorrect++;
+                    }
+                }
+             }  
+                 document.getElementById('ans1').style.display='block';
+                 document.body.scrollTop = 0; // For Safari
+                 document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+                 document.getElementById("quiz").innerHTML = "your mark is:" +  amountCorrect +"/"+ 100;
+                
           }
-         }                   
-         document.body.scrollTop = 0; // For Safari
-         document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-         document.getElementById("quiz").innerHTML = "your mark is:" +  amountCorrect +"/"+ 100;
-        }
-             
